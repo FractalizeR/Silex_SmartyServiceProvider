7420c8479d267bbf080faa16d832774475fae316
Vladislav Rastrusny <FractalizeR@yandex.ru>
2011-10-17 13:50:27 +0400

    First commit

---
+++ src/FractalizeR/Smarty/ServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * ========================================================================
+ * Copyright (c) 2011 Vladislav "FractalizeR" Rastrusny
+ * Website: http://www.fractalizer.ru
+ * Email: FractalizeR@yandex.ru
+ * ------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================================
+ */
+
+namespace FractalizeR\Smarty;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['smarty'] = $app->share(function () use ($app)
+        {
+            if (!isset ($app['smarty.dir'])) {
+                throw new \RuntimeException("'smarty.dir' is not defined. Please provide this option to Application->register() call.");
+            }
+
+            require_once($app['smarty.dir'] . '/libs/Smarty.class.php');
+            $smarty = new \Smarty();
+
+            if (isset($app["smarty.options"])) {
+                foreach ($app["smarty.options"] as $smartyOptionName => $smartyOptionValue) {
+                    $smarty->$smartyOptionName = $smartyOptionValue;
+                }
+            }
+
+            $smarty->assign("app", $app);
+
+            if (isset($app['smarty.configure'])) {
+                $app['smarty.configure']($smarty);
+            }
+
+            return $smarty;
+        });
+    }
+}
+
---
+++ tests/FractalizeR/Smarty/Tests/ServiceProviderTest.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * ========================================================================
+ * Copyright (c) 2011 Vladislav "FractalizeR" Rastrusny
+ * Website: http://www.fractalizer.ru
+ * Email: FractalizeR@yandex.ru
+ * ------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================================
+ */
+
+namespace FractalizeR\Smarty;
+
+use Silex\Application;
+use FractalizeR\Smarty\ServiceProvider as SmartyServiceProvider;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * SmartyExtension test cases
+ *
+ * Smarty 3.0 demo files should be corrected for tests to pass when PHPUnit is run in CLI mode:
+ *
+ * - delete line with {popup_init} from Smarty\demo\templates\header.tpl (should be done already from Smarty 3.0.8 onwards)
+ *
+ * @author Vladislav Rastrusny aka FractalizeR <FractalizeR@yandex.ru>
+ */
+class ServiceProviderTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var string Path to smarty distribution
+     */
+    private $smartyPath;
+
+    public function setUp() {
+        $this->smartyPath = __DIR__ . '/../../../../vendor/Smarty';
+        if (!isset($_SERVER['SERVER_NAME'])) {
+            $_SERVER['SERVER_NAME'] = 'Value for Smarty demo';
+        }
+
+        if (!is_dir($this->smartyPath)) {
+            $this->markTestSkipped('Smarty submodule was not installed.');
+        }
+    }
+
+    public function testRegisterAndRender() {
+        $app = new Application();
+
+        $app->register(new SmartyServiceProvider(), array('smarty.dir' => $this->smartyPath, 'smarty.options' =>
+            array('template_dir' => $this->smartyPath . '/demo/templates',
+                'compile_dir' => $this->smartyPath . '/demo/templates_c',
+                'config_dir' => $this->smartyPath . '/demo/configs',
+                'cache_dir' => $this->smartyPath . '/demo/cache',),));
+
+        $app->get('/hello', function() use ($app) {
+                /** @var \Smarty $smarty  */
+                $smarty = $app['smarty'];
+                $smarty->debugging = false;
+                $smarty->caching = true;
+                $smarty->cache_lifetime = 120;
+
+                $smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
+                $smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
+                $smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
+                $smarty->assign("Class",
+                    array(array("A", "B", "C", "D"), array("E", "F", "G", "H"), array("I", "J", "K", "L"),
+                        array("M", "N", "O", "P")));
+
+                $smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
+                        array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
+
+                $smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
+                $smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
+                $smarty->assign("option_selected", "NE");
+
+                return $smarty->fetch('index.tpl');
+            });
+
+        $request = Request::create('/hello');
+
+        $response = $app->handle($request);
+        $this->assertGreaterThan(7000, strlen($response->getContent()));
+    }
+}
+
+
